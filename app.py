2508774aadee94dc61eea04c83ca500e764a447b
SANDAKA92 <sandakauop92@gmail.com>
2024-10-19 12:37:36 +0530

    improved the job dynamic data

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+JOBS = [
+  {
+ 'id': 1,
+ 'title': 'Computer Programmer',
+ 'location': 'Kelaniya',
+ 'salary': 'Rs. 100000'
+},
+{
+  'id': 2,
+ 'title': 'Computer Programmer2',
+ 'location': 'Kelaniya2',
+ 'salary': 'Rs. 200000'
+},
+{
+  'id': 3,
+ 'title': 'System Analyst',
+ 'location': 'Peradeniya',
+ 'salary': 'Rs. 300000'
+}
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
